beba6369400829eccce498cd172c60293b886500
Luka <lukakataev1@gmail.com>
2026-03-31 13:48:45 +0500

    fix: fixed script file

--- KataevLIB/ApplicationContext.cs
+++ KataevLIB/ApplicationContext.cs
@@ -10,7 +10,7 @@ namespace KataevLIB
 
         public ApplicationContext()
         {
-            Database.EnsureCreated();
+          
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
